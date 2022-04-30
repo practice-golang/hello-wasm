cb650ede43d6b4e1874ba24349880a663c8b3ce7
Practice Golang <practice.golang@gmail.com>
2022-05-01 04:21:44 +0900

    Add hi

--- hello/go.mod
+++ hello/go.mod
@@ -1,3 +1,3 @@
 module hello.wasm
 
-go 1.14
+go 1.18
--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,27 @@
 package main // import "hello.wasm"
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
+
+var g = js.Global()
+
+func Hi(this js.Value, d []js.Value) any {
+	name := "anonymous"
+	if len(d) > 0 {
+		name = d[0].String()
+	}
+	result := "Hi " + name + "!!"
+
+	return result
+}
 
 func main() {
 	fmt.Println("Hello, WebAssembly!")
 	fmt.Println("This is fmt.Println from hello.wasm")
+
+	c := make(chan struct{}, 0)
+	g.Set("hi", js.FuncOf(Hi))
+	<-c
 }
