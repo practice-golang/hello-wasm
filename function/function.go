87cd6e0c2334c255556eae7b3d11ef3a37042308
Practice Golang <practice.golang@gmail.com>
2022-05-06 20:54:54 +0900

    print values from var names

--- function/function.go
+++ function/function.go
@@ -1,10 +1,12 @@
 package main // import "function.wasm"
 
 import (
+	"regexp"
 	"syscall/js"
 )
 
 var g = js.Global()
+var reFuncExist = regexp.MustCompile(`[(|)]+`)
 
 func Hello(this js.Value, d []js.Value) any {
 	name := "anonymous"
@@ -32,9 +34,32 @@ func getValue(this js.Value, d []js.Value) any {
 	}
 
 	varName := d[0].String()
+	matches := reFuncExist.FindStringSubmatch(varName)
+
+	if len(matches) > 0 {
+		return js.Undefined()
+	}
 
 	// f := g.Call("eval", varName+"")
 	f1 := g.Call("Function", "'use strict'; return "+varName)
+	f2 := f1.Invoke()
+
+	return f2
+}
+
+func callFunction(this js.Value, d []js.Value) any {
+	if len(d) < 1 {
+		return "undefined"
+	}
+
+	cmdString := d[0].String()
+	matches := reFuncExist.FindStringSubmatch(cmdString)
+
+	if len(matches) == 0 {
+		return js.Undefined()
+	}
+
+	f1 := g.Call("Function", "'use strict'; return "+cmdString)
 	f2 := f1.Invoke()
 
 	return f2
@@ -45,5 +70,6 @@ func main() {
 	g.Set("hello", js.FuncOf(Hello))
 	g.Set("sum", js.FuncOf(Sum))
 	g.Set("getvalue", js.FuncOf(getValue))
+	g.Set("callfunction", js.FuncOf(callFunction))
 	<-c
 }
--- wasm_exec_tinygo.js
+++ wasm_exec_tinygo.js
@@ -310,6 +310,14 @@
 					"syscall/js.finalizeRef": (sp) => {
 						// Note: TinyGo does not support finalizers so this should never be
 						// called.
+						const id = mem().getUint32(sp, true);
+						this._goRefCounts[id]--;
+						if (this._goRefCounts[id] === 0) {
+							const v = this._values[id];
+							this._values[id] = null;
+							this._ids.delete(v);
+							this._idPool.push(id);
+						}
 						// console.error('syscall/js.finalizeRef not implemented');
 					},
 
